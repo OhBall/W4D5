37b80ccb3a7f45cd9ce7253d6748d1ecff1baf67
Rowan Littlefield <rowanlittlefield@ymail.com>
2018-06-15 15:41:31 -0400

    Imlement auth controllers

--- goal_setter/app/controllers/sessions_controller.rb
+++ goal_setter/app/controllers/sessions_controller.rb
@@ -1,3 +1,27 @@
 class SessionsController < ApplicationController
+  def new 
+    render :new
+  end
+  
+  def create
+    user = User.find_by_credentials(session_params)
+    if user
+      log_in(user)
+      redirect_to user_url(user)
+    else
+      flash.now[:errors] = ["Invalid email/password"]
+      render :new
+    end
+  end
+  
+  def destroy
+    logout
+    redirect_to new_session_url
+  end
+  
+  private
+  def session_params
+    params.require(:session).permit(:email, :password)
+  end
   
 end
